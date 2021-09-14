f970ddc51267b540e9249310dd925e0331b105be
BrokenEagle <BrokenEagle98@yahoo.com>
2021-09-14 00:34:29 -0700

    Allow flask environment to be set with initialize batch file

--- misc/initialize.bat
+++ misc/initialize.bat
@@ -2,5 +2,14 @@
 
 echo ----Setting Environment Variables----
 set FLASK_APP=prebooru
+
+if "%~1"=="development" goto development
+
+set FLASK_ENV=production
+goto end
+
+:development
 set FLASK_ENV=development
+
+:end
 echo.
