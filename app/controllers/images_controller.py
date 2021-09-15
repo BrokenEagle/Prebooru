99178721a4afd8fb98981e2a789ebdd7fcf354bb
BrokenEagle <BrokenEagle98@yahoo.com>
2021-09-14 17:20:33 -0700

    Standardize controller layout

--- app/controllers/images_controller.py
+++ app/controllers/images_controller.py
@@ -1,29 +1,22 @@
-# APP\CONTROLLERS\ILLUSTS.PY
+# APP\CONTROLLERS\IMAGES_CONTROLLER.PY
 
 # ## PYTHON IMPORTS
-from flask import Blueprint, request, render_template, send_from_directory
+from flask import Blueprint, send_from_directory
 
 # ## LOCAL IMPORTS
-from ..models import ArtistUrl
-from .base_controller import GetParamsValue, ProcessRequestValues, ShowJson, IndexJson, SearchFilter, DefaultOrder, Paginate, GetOrAbort
-from app.storage import IMAGE_DIRECTORY
+from ..storage import IMAGE_DIRECTORY
 
 # ## GLOBAL VARIABLES
-from ..storage import IMAGE_DIRECTORY
 
 bp = Blueprint("images", __name__)
 
 
 # ## FUNCTIONS
 
-# #### Route helpers
-
 # #### Route functions
 
-# ###### SHOW
+# ###### MISC
 
 @bp.route('/images/<path:path>')
 def send_file(path):
     return send_from_directory(IMAGE_DIRECTORY, path)
-
-# ###### INDEX
