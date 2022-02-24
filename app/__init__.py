f3e4745ab814fdc9e6ad5f4c4e0dc5b5bf72762e
BrokenEagle <BrokenEagle98@yahoo.com>
2022-02-24 08:25:15 -0800

    Fix print from mangled wrapping due to concurrency

--- app/__init__.py
+++ app/__init__.py
@@ -99,7 +99,7 @@ class MethodRewriteMiddleware(object):
 
     def __call__(self, environ, start_response):
         SERVER_INFO.addr = environ['HTTP_HOST'].split(':')[0]
-        print("Server addr: %s" % SERVER_INFO.addr, flush=True)
+        print(f"\nMiddleware: Server addr - {SERVER_INFO.addr}\n", end="", flush=True)
         if environ['REQUEST_METHOD'] == 'POST':
             environ['wsgi.input'] = stream = BytesIO(get_input_stream(environ).read())
             _, form, _ = parse_form_data(environ)
