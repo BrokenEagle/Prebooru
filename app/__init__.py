5663d9aa5e9e112ef435e30beac409e1874a322d
BrokenEagle <BrokenEagle98@yahoo.com>
2022-03-10 20:43:04 -0800

    Add exception guard to updating the last activity before the request
    An exception during this update would cause the entire process to fail,
    and so the teardown request handler would never execute. This was first
    seen when the database was locked for a long time, which happend during
    a vaccum/analyze cycle.

--- app/__init__.py
+++ app/__init__.py
@@ -73,7 +73,11 @@ def _before_request():
     msg = f"\nBefore request: Allow - {SERVER_INFO.allow_requests}, Active = {SERVER_INFO.active_requests}\n"
     print(msg, end="", flush=True)
     SERVER_INFO.active_requests += 1
-    update_last_activity()
+    try:
+        update_last_activity()
+    except Exception as e:
+        # Don't fail the request if the database is locked
+        print(f"Unable to update last activity:\r\n {e}")
     return None if SERVER_INFO.allow_requests else ""
 
 
