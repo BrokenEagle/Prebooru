47551a7f5a920a21c0334b03ca690418c3ff2782
BrokenEagle <BrokenEagle98@yahoo.com>
2022-04-05 20:32:42 -0700

    Fix favicon error when viewing the JSON in a browser page

--- app/controllers/static_controller.py
+++ app/controllers/static_controller.py
@@ -1,15 +1,28 @@
 # APP/CONTROLLERS/STATIC_CONTROLLER.PY
 
+# ## PYTHON IMPORTS
+import os
+
 # ## EXTERNAL IMPORTS
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory
+
+# ## LOCAL IMPORTS
+from .. import PREBOORU_APP
 
 
 # ## GLOBAL VARIABLES
 
 bp = Blueprint("static", __name__)
 
+STATIC_PATH = os.path.join(PREBOORU_APP.root_path, 'app', 'static')
+
 
 # ## FUNCTIONS
+
+@PREBOORU_APP.route('/favicon.ico')
+def favicon():
+    return send_from_directory(STATIC_PATH, 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 
 @bp.route('/static/site_map', methods=['GET'])
 def site_map_html():
