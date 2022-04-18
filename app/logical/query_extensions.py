7941129641e143079f49e4f2e733439000f75ec9
BrokenEagle <BrokenEagle98@yahoo.com>
2022-04-17 21:49:51 -0700

    Extend query with a limit paginate
    This is for query loops where elements being queried for change during
    the processing of the loop, therefore changing the boundaries of the
    pages in the process, which causes items to be missed.

--- app/logical/query_extensions.py
+++ app/logical/query_extensions.py
@@ -59,6 +59,66 @@ class CountPaginate():
         return self._count_query.get_count()
 
 
+class LimitPaginate():
+    """Pagination method for processing items which may be removed from the total as it gets processed."""
+    def __init__(self, query=None, page=1, per_page=20, count=None, next_id=None, prev_id=None):
+        self.query = query
+        self.per_page = per_page
+        self.page = page
+        self.next_id = next_id
+        self.prev_id = prev_id
+        self.items = self._GetItems()
+        self.current_count = self._GetCount()
+        self.count = count or self.current_count
+        self.pages = ((self.current_count - 1) // per_page) + 1
+        self.first = ((page - 1) * per_page) + 1
+        self.last = min(page * per_page, self.count)
+
+    @property
+    def has_next(self):
+        return self.pages > 0 and len(self.items) > self.per_page
+
+    @property
+    def has_prev(self):
+        return self.pages > 0 and len(self.items) > self.per_page
+
+    @property
+    def next_num(self):
+        return self.page + 1
+
+    @property
+    def prev_num(self):
+        return self.page - 1
+
+    @property
+    def next_sequential_id(self):
+        return min([item.id for item in self.items]) if len(self.items) else None
+
+    @property
+    def prev_sequential_id(self):
+        return max([item.id for item in self.items]) if len(self.items) else None
+
+    def next(self):
+        return LimitPaginate(query=self.query, page=self.next_num, per_page=self.per_page, count=self.count,
+                             next_id=self.next_sequential_id) if self.has_next else None
+
+    def prev(self):
+        return LimitPaginate(query=self.query, page=self.prev_num, per_page=self.per_page, count=self.count,
+                             prev_id=self.prev_sequential_id) if self.has_prev else None
+
+    def _GetItems(self):
+        model = _query_model(self.query)
+        q = self.query
+        if self.next_id:
+            q = q.filter(model.id < self.next_id)
+        elif self.prev_id:
+            q = q.filter(model.id > self.prev_id)
+        return q.limit(self.per_page).all()
+
+    def _GetCount(self):
+        return self.query.count()
+
+
 # ## FUNCTIONS
 
 # #### Initialization functions
@@ -71,6 +131,7 @@ def initialize():
         sqlalchemy.orm.Query.unique_join = unique_join
         sqlalchemy.orm.Query.get_count = get_count
         sqlalchemy.orm.Query.count_paginate = count_paginate
+        sqlalchemy.orm.Query.limit_paginate = limit_paginate
         INIT = True
 
 
@@ -93,8 +154,16 @@ def count_paginate(self, page=1, per_page=20):
     return CountPaginate(query=self, page=page, per_page=per_page)
 
 
+def limit_paginate(self, page=1, per_page=20):
+    return LimitPaginate(query=self, page=page, per_page=per_page)
+
+
 # #### Private functions
 
 def _has_entity(self, model):
     current_joined_tables = [t[0] for t in self._legacy_setup_joins]
     return model.__table__ in current_joined_tables
+
+
+def _query_model(query):
+    return query.column_descriptions[0]['entity']
