1277c2f840cf6548893292beb2af921a3216bc71
BrokenEagle <BrokenEagle98@yahoo.com>
2022-07-20 18:37:18 -0700

    Fix similarity elements on post show page
    - Use the HTML delete link instead of the JSON delete link for now.
    - Remove the similar_posts.js file, as it was never used.

--- app/static/javascript/similarity_posts.js
+++
@@ -1,8 +0,0 @@
-const SimilarityPosts = {};
-
-SimilarityPosts.deleteElement = function(obj) {
-    if (confirm('Remove element from similar posts?')) {
-        Prebooru.linkDelete(obj);
-    }
-    return false;
-};
