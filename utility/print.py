7a8a5b20fa543a14a8f9684f4f6852a811a7f6f0
BrokenEagle <BrokenEagle98@yahoo.com>
2022-08-11 21:50:34 -0700

    Update print functions
    Rename warn to warning, and allow the trim to be optional.

--- utility/print.py
+++ utility/print.py
@@ -8,26 +8,31 @@ import traceback
 
 # ## FUNCTIONS
 
-def safe_print(*args, **kwargs):
-    print(_coalesce_arguments(args).encode('ascii', 'backslashreplace').decode(), **kwargs)
+def normal_print(*args, trim=False, **kwargs):
+    print(_coalesce_arguments(args, trim), **kwargs)
 
 
-def print_info(*args, **kwargs):
-    print(colorama.Fore.GREEN + colorama.Style.BRIGHT + _coalesce_arguments(args) + colorama.Style.RESET_ALL, **kwargs)
+def safe_print(*args, trim=False, **kwargs):
+    print(_coalesce_arguments(args, trim).encode('ascii', 'backslashreplace').decode(), **kwargs)
 
 
-def print_warn(*args, **kwargs):
-    print(colorama.Fore.YELLOW + colorama.Style.BRIGHT + _coalesce_arguments(args) + colorama.Style.RESET_ALL, **kwargs)
+def print_info(*args, trim=False, **kwargs):
+    print(colorama.Fore.GREEN + colorama.Style.BRIGHT + _coalesce_arguments(args, trim) + colorama.Style.RESET_ALL, **kwargs)
 
 
-def print_error(*args, **kwargs):
-    print(colorama.Fore.RED + colorama.Style.BRIGHT + _coalesce_arguments(args) + colorama.Style.RESET_ALL, **kwargs)
+def print_warning(*args, trim=False, **kwargs):
+    print(colorama.Fore.YELLOW + colorama.Style.BRIGHT + _coalesce_arguments(args, trim) + colorama.Style.RESET_ALL, **kwargs)
 
 
-def buffered_print(name, safe=False, header=True, sep=" ", end="\n"):
+def print_error(*args, trim=False, **kwargs):
+    print(colorama.Fore.RED + colorama.Style.BRIGHT + _coalesce_arguments(args, trim) + colorama.Style.RESET_ALL, **kwargs)
+
+
+def buffered_print(name, safe=False, header=True, trim=False, sep=" ", end="\n"):
     buffer_key = name + " - " + uuid.uuid1().hex
     if header:
         print("\n++++++++++ %s ++++++++++\n" % buffer_key, flush=True)
+    print_func = safe_print if safe else normal_print
     print_buffer = []
 
     def accumulator(*args):
@@ -40,7 +45,7 @@ def buffered_print(name, safe=False, header=True, sep=" ", end="\n"):
         print_buffer = ["\n", top_header, "\n"] + print_buffer
         print_buffer += [("=" * len(top_header)), "\n"]
         print_str = sep.join(map(str, print_buffer))
-        print_func(print_str, flush=True)
+        print_func(print_str, trim=trim, flush=True)
 
     setattr(accumulator, 'print', printer)
     return accumulator
@@ -53,11 +58,11 @@ def exception_print(error):
 
 # #### Private
 
-def _coalesce_arguments(args):
+def _coalesce_arguments(args, trim):
     temp = ''
     for arg in args:
         if type(arg) is str:
             temp += arg + ' '
         else:
             temp += repr(arg) + ' '
-    return temp.strip()
+    return temp.strip() if trim else temp
