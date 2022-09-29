180243d122baaca1acf564bd5febe8966611b77e
BrokenEagle <BrokenEagle98@yahoo.com>
2022-09-29 12:42:08 -0700

    Add migration helper to initialize column from existing data

--- migrations/columns.py
+++ migrations/columns.py
@@ -44,6 +44,35 @@ def alter_column(table_name, column_name, column_type, alter_args):
     alter_columns(table_name, [(column_name, column_type, alter_args)])
 
 
+def initialize_column(table_name, column_name, column_type, *extra_columns):
+    columns = [sa.Column('id', sa.Integer), sa.Column(column_name, getattr(sa, column_type))]
+    for (colname, coltype) in extra_columns:
+        columns.append(sa.Column(colname, getattr(sa, coltype)))
+    t = sa.Table(
+        table_name,
+        sa.MetaData(),
+        *columns,
+    )
+    select = [t.c.id, getattr(t.c, column_name)]
+    for (colname, _) in extra_columns:
+        select.append(getattr(t.c, colname))
+    connection = op.get_bind()
+    index = 0
+    while True:
+        statement = sa.select(select).limit(1000).offset(index * 1000)
+        data = connection.execute(statement).fetchall()
+        for item in data:
+            (id, value, *extra_values) = item
+            def _update(**kwargs):
+                connection.execute(t.update().where(t.c.id == id).values(**kwargs))
+            yield id, value, _update, {extra_columns[i][0]: extra_values[i] for i in range(len(extra_values))}
+        if len(data):
+            print(f"{table_name} #{data[0][0]} - {table_name} #{data[-1][0]}")
+        if len(data) < 1000:
+            return
+        index += 1
+
+
 def transfer_column(table_name, from_column, to_column):
     from_name, from_type = from_column
     to_name, to_type = to_column
