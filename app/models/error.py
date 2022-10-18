68ced8e483c41f7b6071fa5f8c70d9ead4a18564
BrokenEagle <BrokenEagle98@yahoo.com>
2022-10-18 15:16:57 -0700

    Add function on error to combine all append items into one

--- app/models/error.py
+++ app/models/error.py
@@ -1,5 +1,8 @@
 # APP/MODELS/ERROR.PY
 
+# ## EXTERNAL IMPORTS
+from sqlalchemy.util import memoized_property
+
 # ## LOCAL IMPORTS
 from .. import DB
 from .base import JsonModel, NormalizedDatetime
@@ -15,3 +18,7 @@ class Error(JsonModel):
     module = DB.Column(DB.String(255), nullable=False)
     message = DB.Column(DB.UnicodeText, nullable=False)
     created = DB.Column(NormalizedDatetime(), nullable=False)
+
+    @memoized_property
+    def append_item(self):
+        return self.upload or self.upload_element or self.post or self.subscription or self.subscription_element
