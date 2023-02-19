06090f024653a528489b512ece70b3311d04e609
BrokenEagle <BrokenEagle98@yahoo.com>
2023-02-18 16:01:57 -0800

    Add additional logger functions

--- app/logical/logger.py
+++ app/logical/logger.py
@@ -10,6 +10,7 @@ import traceback
 from config import DATA_DIRECTORY
 from utility.file import load_default, put_get_json
 from utility.uprint import print_error
+from utility.data import merge_dicts
 
 
 # ## GLOBAL VARIABLES
@@ -22,8 +23,7 @@ NETWORK_ERROR_LOG_FILE = os.path.join(DATA_DIRECTORY, 'network_error_log.json')
 
 def log_error(module, message):
     all_errors = load_default(ERROR_LOG_FILE, [])
-    exc_type, exc_value, exc_tb = sys.exc_info()
-    error = traceback.format_exception(exc_type, exc_value, exc_tb)
+    error = get_traceback()
     all_errors.append({
         'module': module,
         'message': message,
@@ -49,3 +49,14 @@ def log_network_error(module, response):
         'time': time.ctime(),
     })
     put_get_json(NETWORK_ERROR_LOG_FILE, 'w', all_errors)
+
+
+def handle_error_message(message, retdata=None):
+    retdata = retdata or {}
+    print_error(message)
+    return merge_dicts({'error': True, 'message': message}, retdata)
+
+
+def get_traceback():
+    exc_type, exc_value, exc_tb = sys.exc_info()
+    return traceback.format_exception(exc_type, exc_value, exc_tb)
