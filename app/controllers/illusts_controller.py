952a3c10ccca20440c2a65950367822be956364b
BrokenEagle <BrokenEagle98@yahoo.com>
2023-09-02 03:01:07 -0700

    Support `site_name` for illust creation
    Since the ID values aren't constant, whereas the names are.

--- app/controllers/illusts_controller.py
+++ app/controllers/illusts_controller.py
@@ -214,11 +214,15 @@ def convert_create_params(dataparams):
     set_default(createparams, 'pages', 1)
     set_default(createparams, 'score', 0)
     set_default(createparams, 'tags', [])
+    if 'site_name' in dataparams and dataparams['site_name'] in site_descriptor.names:
+        createparams['site_id'] = site_descriptor.by_name(dataparams['site_name']).id
     if 'illust_urls' in dataparams:
         # Arrays of hashes are sent as a hash where each index is a key
         createparams['illust_urls'] = [v for v in dataparams['illust_urls'].values()]
         for url_data in createparams['illust_urls']:
             url_data['active'] = parse_bool_parameter(url_data, 'active')
+            if 'site_name' in url_data:
+                url_data['site_id'] = site_descriptor.by_name(url_data['site_name']).id
     createparams['commentaries'] = [dataparams['commentary']] if len(dataparams['commentary']) else None
     return createparams
 
