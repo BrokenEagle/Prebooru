04e70670ac14a019908fc346980b62ded12d386d
BrokenEagle <BrokenEagle98@yahoo.com>
2024-02-08 19:39:05 -0800

    Add some parameter checking and fixing for Twitter subscriptions

--- app/logical/sources/twitter_src.py
+++ app/logical/sources/twitter_src.py
@@ -1503,6 +1503,10 @@ def populate_all_artist_illusts(artist, job_id=None, params=None):
             'type': 'media',
             'last_id': artist.last_illust_id,
         }
+    elif params['type'] == 'search':
+        params['last_id'] = None
+    if params['type'] == 'search' and params['search_since'] is None and params['search_until'] is None and params['last_id'] is None:
+        raise Exception("Invalid process parameters for search timeline.")
     if params['type'] == 'media':
         return populate_artist_illusts_from_media_timeline(artist, job_id, params['last_id'])
     if params['type'] == 'search':
