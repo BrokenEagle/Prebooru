b48c467780970843d3c172589c7cb4be54368a30
BrokenEagle <BrokenEagle98@yahoo.com>
2024-11-21 10:02:03 -0800

    Make a an HTML function specifically for notations
    This allows file links to be included, which aren't needed for any
    other text record.

--- app/helpers/notations_helper.py
+++ app/helpers/notations_helper.py
@@ -7,6 +7,14 @@ import html
 # ## EXTERNAL IMPORTS
 from flask import Markup
 
+# ## LOCAL IMPORTS
+from .base_helper import general_link, convert_to_html
+
+
+# ## GLOBAL VARIABLES
+
+FILEPATH_RG = re.compile(r'^[C-Z]:\\.*(?:jpg|gif|png)$', re.IGNORECASE)
+
 
 # ## FUNCTIONS
 
@@ -22,3 +30,14 @@ def edit_append_info(notation):
         return Markup(f"For {notation.append_item.pool.name_link}:")
     else:
         return Markup(f"For {notation.append_item.show_link}:")
+
+
+def to_html(notation):
+    lines = re.split(r'\r?\n', notation.body)
+    for i, line in enumerate(lines):
+        if FILEPATH_RG.match(line):
+            addons = {'onclick': 'return Prebooru.copyFileLink(this)', 'data-file-path': line, 'title': "Click to copy.", 'class': 'file-link'}
+            lines[i] =  Markup(general_link(line, "#", **addons))
+        else:
+            lines[i] = convert_to_html(line)
+    return Markup("<br>").join(lines)
