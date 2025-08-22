9e45629bdfeebac83ab41b49b179248a69873eb4
BrokenEagle <BrokenEagle98@yahoo.com>
2025-08-22 15:01:51 -0700

    Update download elements display on downloads SHOW
    Add function to redownload an element which has errored out.

--- app/helpers/downloads_helper.py
+++ app/helpers/downloads_helper.py
@@ -12,6 +12,19 @@ from .base_helper import general_link, add_container
 
 # #### Downloads
 
+# ###### URL functions
+
+def post_search(download):
+    params = {
+        'illust_urls': {
+            'download_elements': {
+                'download_id': download.id,
+            },
+        },
+    }
+    return search_url_for('post.index_html', **params)
+
+
 # ###### Link functions
 
 def check_pending_link(download):
@@ -23,14 +36,15 @@ def resubmit_link(download):
 
 
 def post_search_link(download):
-    params = {
-        'illust_urls': {
-            'download_elements': {
-                'download_id': download.id,
-            },
-        },
-    }
-    return general_link('»', search_url_for('post.index_html', **params))
+    return general_link('»', post_search(download))
+
+
+def element_search_link(download):
+    return general_link('»', search_url_for('download_element.index_html', download_id=download.id))
+
+
+def redownload_link(element):
+    return general_link('redownload', url_for('download_element.redownload_html', id=element.id), method="POST")
 
 
 # #### Download elements
