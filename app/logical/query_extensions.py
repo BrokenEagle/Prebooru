f15f24481b87664a4620a521c229a508920e9b22
BrokenEagle <BrokenEagle98@yahoo.com>
2025-08-23 13:11:49 -0700

    Add sequential pagination
    This will be used to replace the limit pagination, which is flawed
    in some cases which prevents all of the items from being found.

--- app/logical/query_extensions.py
+++ app/logical/query_extensions.py
@@ -1,6 +1,7 @@
 # APP/LOGICAL/QUERY_EXTENSIONS.PY
 
 # ## PYTHON IMPORTS
+import re
 from types import SimpleNamespace
 from functools import reduce
 
@@ -66,6 +67,109 @@ class CountPaginate():
         else:
             q = self.query
         return q.limit(self.per_page).offset(self.offset).all()
+
+    def _get_count(self):
+        # Easy way to get an exact copy of a query
+        self._count_query = self.query.filter()
+        if len(self._count_query._where_criteria) == 0:
+            model = self._count_query.column_descriptions[0]['entity']
+            # Queries with no where criteria do not work correctly
+            self._count_query = self._count_query.filter(*model.pk_cols)
+        # Using function count with scalar does not like loader options
+        self._count_query._with_options = ()
+        if self.distinct:
+            # Keep it from rendering an unncessary DISTINCT outside of the count
+            self._count_query._distinct_on = ()
+            self._count_query._distinct = False
+        return self._count_query.get_count() if not self.distinct else self._count_query.distinct_count()
+
+
+class SequentialPaginate():
+    def __init__(self, query=None, per_page=DEFAULT_PAGINATE_LIMIT, page=None, count=None, distinct=False,
+                 min_id=None, max_id=None):
+        self.query = query
+        self.per_page = per_page
+        self.distinct = distinct
+        self.current_count = self._get_count()
+        self.count = count or self.current_count
+        if self.current_count > 0:
+            self.min_id = min_id or self._get_min_id()
+            self.max_id = max_id or self._get_max_id()
+            if page == 'oldest_first':
+                self.direction = 'a'
+                self.sequential_id = self.min_id - 1
+            elif page == 'newest_first':
+                self.direction = 'b'
+                self.sequential_id = self.max_id + 1
+            elif isinstance(page, str):
+                match = re.match(r'^(a|b)(\d+)$', page)
+                if match:
+                    self.direction = match.group(1)
+                    self.sequential_id = int(match.group(2))
+                else:
+                    raise Exception("Invalid sequential page number specified.")
+            else:
+                raise Exception("Invalid page specified.")
+            self.items = self._get_items() if self.current_count > 0 else []
+            self.page_min, self.page_max =\
+                [self.items[0], self.items[-1]]\
+                if self.direction == 'a' else\
+                [self.items[-1], self.items[0]]
+            if self.page_max.id == self.page_min.id:
+                self.range = f'{self.page_min.shortlink}'
+            elif self.direction == 'a':
+                self.range = f'{self.page_min.shortlink} - {self.page_max.shortlink}'
+            else:
+                self.range = f'{self.page_max.shortlink} - {self.page_min.shortlink}'
+        else:
+            self.min_id = self.max_id = self.page_min = self.page_max = None
+            self.items = []
+            self.range = 'N/A'
+
+    @property
+    def above_pagenum(self):
+        return f'a{self.page_max.id}' if self.current_count > 0 else None
+
+    @property
+    def below_pagenum(self):
+        return f'b{self.page_min.id}' if self.current_count > 0 else None
+
+    @property
+    def has_above(self):
+        return self.max_id > self.page_max.id if self.current_count > 0 else False
+
+    @property
+    def has_below(self):
+        return self.min_id < self.page_min.id if self.current_count > 0 else False
+
+    def above(self):
+        return SequentialPaginate(query=self.query, per_page=self.per_page, page=self.above_pagenum, count=self.count,
+                                  min_id=self.min_id, max_id=self.max_id, distinct=self.distinct)
+
+    def below(self):
+        return SequentialPaginate(query=self.query, per_page=self.per_page, page=self.below_pagenum, count=self.count,
+                                  min_id=self.min_id, max_id=self.max_id, distinct=self.distinct)
+
+    def _get_min_id(self):
+        model = _query_model(self.query)
+        return self.query.with_entities(model.id).order_by(model.id.asc()).limit(1).scalar()
+
+    def _get_max_id(self):
+        model = _query_model(self.query)
+        return self.query.with_entities(model.id).order_by(model.id.desc()).limit(1).scalar()
+
+    def _get_items(self):
+        model = _query_model(self.query)
+        if self.distinct:
+            model = self.query.column_descriptions[0]['entity']
+            q = self.query.group_by(*model.pk_cols)
+        else:
+            q = self.query
+        if self.direction == 'a':
+            q = q.filter(model.id > self.sequential_id).order_by(model.id.asc())
+        else:
+            q = q.filter(model.id < self.sequential_id).order_by(model.id.desc())
+        return q.limit(self.per_page).all()
 
     def _get_count(self):
         # Easy way to get an exact copy of a query
@@ -171,6 +275,7 @@ def initialize():
         sqlalchemy.orm.Query.relation_count = relation_count
         sqlalchemy.orm.Query.distinct_count = distinct_count
         sqlalchemy.orm.Query.count_paginate = count_paginate
+        sqlalchemy.orm.Query.sequential_paginate = sequential_paginate
         sqlalchemy.orm.Query.limit_paginate = limit_paginate
         sqlalchemy.orm.Query.all2 = secondary_all
         sqlalchemy.orm.Query.first2 = secondary_first
@@ -216,6 +321,10 @@ def distinct_count(self):
 
 def count_paginate(self, **kwargs):
     return CountPaginate(query=self, **kwargs)
+
+
+def sequential_paginate(self, **kwargs):
+    return SequentialPaginate(query=self, **kwargs)
 
 
 def limit_paginate(self, page=1, per_page=DEFAULT_PAGINATE_LIMIT, distinct=False):
