8a4542df6ec0d8a280522f2aa57b8eed9affecab
BrokenEagle <BrokenEagle98@yahoo.com>
2025-08-23 18:17:45 -0700

    Remove limit paginate

--- app/logical/query_extensions.py
+++ app/logical/query_extensions.py
@@ -187,79 +187,6 @@ class SequentialPaginate():
         return self._count_query.get_count() if not self.distinct else self._count_query.distinct_count()
 
 
-class LimitPaginate():
-    """Pagination method for processing items which may be removed from the total as it gets processed."""
-    def __init__(self, query=None, page=1, per_page=DEFAULT_PAGINATE_LIMIT, count=None, distinct=False, next_id=None,
-                 prev_id=None):
-        self.query = query
-        self.per_page = per_page
-        self.page = page
-        self.next_id = next_id
-        self.prev_id = prev_id
-        self.distinct = distinct
-        self.current_count = self._get_count()
-        self.count = count or self.current_count
-        self.items = self._get_items() if self.current_count > 0 else []
-        self.pages = ((self.current_count - 1) // per_page) + 1
-        self.total = ((self.count - 1) // per_page) + 1
-        self.first = ((page - 1) * per_page) + 1
-        self.last = min(page * per_page, self.count)
-        self.next_sequential_id = min([item.id for item in self.items]) if len(self.items) else None
-        self.prev_sequential_id = max([item.id for item in self.items]) if len(self.items) else None
-
-    @property
-    def has_next(self):
-        return self.pages > 0 and len(self.items) >= self.per_page
-
-    has_prev = has_next
-
-    @property
-    def next_num(self):
-        return self.page + 1
-
-    prev_num = next_num
-
-    @property
-    def prev_num(self):
-        return self.page + 1
-
-    def next(self):
-        return LimitPaginate(query=self.query, page=self.next_num, per_page=self.per_page, count=self.count,
-                             next_id=self.next_sequential_id, distinct=self.distinct) if self.has_next else None
-
-    def prev(self):
-        return LimitPaginate(query=self.query, page=self.prev_num, per_page=self.per_page, count=self.count,
-                             prev_id=self.prev_sequential_id, distinct=self.distinct) if self.has_prev else None
-
-    def _get_items(self):
-        model = _query_model(self.query)
-        if self.distinct:
-            model = self.query.column_descriptions[0]['entity']
-            q = self.query.group_by(*model.pk_cols)
-        else:
-            q = self.query
-        if self.next_id:
-            q = q.filter(model.id < self.next_id)
-        elif self.prev_id:
-            q = q.filter(model.id > self.prev_id)
-        return q.order_by(model.id.desc()).limit(self.per_page).all()
-
-    def _get_count(self):
-        # Easy way to get an exact copy of a query
-        self._count_query = self.query.filter()
-        if len(self._count_query._where_criteria) == 0:
-            model = self._count_query.column_descriptions[0]['entity']
-            # Queries with no where criteria do not work correctly
-            self._count_query = self._count_query.filter(*model.pk_cols)
-        # Using function count with scalar does not like loader options
-        self._count_query._with_options = ()
-        if self.distinct:
-            # Keep it from rendering an unncessary DISTINCT outside of the count
-            self._count_query._distinct_on = ()
-            self._count_query._distinct = False
-        return self._count_query.get_count() if not self.distinct else self._count_query.distinct_count()
-
-
 # ## FUNCTIONS
 
 # #### Initialization functions
@@ -276,7 +203,6 @@ def initialize():
         sqlalchemy.orm.Query.distinct_count = distinct_count
         sqlalchemy.orm.Query.count_paginate = count_paginate
         sqlalchemy.orm.Query.sequential_paginate = sequential_paginate
-        sqlalchemy.orm.Query.limit_paginate = limit_paginate
         sqlalchemy.orm.Query.all2 = secondary_all
         sqlalchemy.orm.Query.first2 = secondary_first
         sqlalchemy.orm.Query.one2 = secondary_one
@@ -325,10 +251,6 @@ def count_paginate(self, **kwargs):
 
 def sequential_paginate(self, **kwargs):
     return SequentialPaginate(query=self, **kwargs)
-
-
-def limit_paginate(self, page=1, per_page=DEFAULT_PAGINATE_LIMIT, distinct=False):
-    return LimitPaginate(query=self, page=page, per_page=per_page, distinct=distinct)
 
 
 def secondary_all(self):
