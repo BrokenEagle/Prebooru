BrokenEagle/Prebooru

commit ad7956bef64b13b2033038e729f1ac8163db2289

Make md5 the primary link between illust URLs and posts
This allows the post_id to be removed from illust URLs, and illusts
to be removed from archive posts. This automatically relinks posts
and illust URLs when either is reintantiated.

Since neither the post md5s nor the illust URL md5s are guaranteed
to be subsets of each other, no foreign keys can be added to the
database schema, which is why the relationships have to be defined
explicitly in the model files.

As a side benefit of all this, archive posts are now linked to illust
URLs, which allows them to be easily used in places such as on
subscription elements.

app/models/post.py
@@ -14,13 +14,12 @@ from sqlalchemy.ext.associationproxy import association_proxy
 # ## PACKAGE IMPORTS
 from config import MEDIA_DIRECTORY, ALTERNATE_MEDIA_DIRECTORY, PREVIEW_DIMENSIONS, SAMPLE_DIMENSIONS
 from utility.obj import memoized_classproperty
-from utility.data import swap_list_values, dict_prune, dict_filter
+from utility.data import swap_list_values, dict_filter
 
 # ## LOCAL IMPORTS
 from ..logical.utility import unique_objects
 from .model_enums import PostType
 from .error import Error
-from .illust_url import IllustUrl
 from .subscription_element import SubscriptionElement
 from .notation import Notation
 from .tag import UserTag, user_tag_creator
@@ -68,7 +67,6 @@ class Post(JsonModel):
     simcheck = boolean_column(nullable=False)
 
     # ## Relationships
-    illust_urls = relationship(IllustUrl, uselist=True, backref=backref('post', uselist=False))
     errors = relationship(Error, uselist=True, cascade='all,delete', backref=backref('post', uselist=False))
     subscription_element = relationship(SubscriptionElement, uselist=False, backref=backref('post', uselist=False))
     notations = relationship(Notation, uselist=True, cascade='all,delete', backref=backref('post', uselist=False))
@@ -176,6 +174,7 @@ class Post(JsonModel):
 
     @memoized_property
     def related_posts(self):
+        from .illust_url import IllustUrl
         from .illust import Illust
         query = Post.query.join(IllustUrl, Post.illust_urls)
         query = query.filter(IllustUrl.illust_id.in_(self._illust_query.with_entities(Illust.id)))
@@ -222,10 +221,6 @@ class Post(JsonModel):
         return query.limit(10).all()
 
     @property
-    def illust_urls_json(self):
-        return [dict_prune(illust_url.to_json(), ['id', 'post_id']) for illust_url in self.illust_urls]
-
-    @property
     def errors_json(self):
         return [dict_filter(error.to_json(), ['module', 'message', 'created']) for error in self.errors]
 
@@ -264,23 +259,26 @@ class Post(JsonModel):
 
     @property
     def _illust_query(self):
+        from .illust_url import IllustUrl
         from .illust import Illust
-        return Illust.query.join(IllustUrl, Illust.urls).filter(IllustUrl.post_id == self.id).group_by(Illust.id)
+        return Illust.query.join(IllustUrl, Illust.urls).filter(IllustUrl.md5 == self.md5).group_by(Illust.id)
 
     @property
     def _artist_query(self):
+        from .illust_url import IllustUrl
         from .illust import Illust
         from .artist import Artist
         return Artist.query.join(Illust, Artist.illusts).join(IllustUrl, Illust.urls)\
-                           .filter(IllustUrl.post_id == self.id).group_by(Artist.id)
+                           .filter(IllustUrl.md5 == self.md5).group_by(Artist.id)
 
     @property
     def _booru_query(self):
+        from .illust_url import IllustUrl
         from .illust import Illust
         from .artist import Artist
         from .booru import Booru
         return Booru.query.join(Artist, Booru.artists).join(Illust, Artist.illusts).join(IllustUrl, Illust.urls)\
-                          .filter(IllustUrl.post_id == self.id).group_by(Booru.id)
+                          .filter(IllustUrl.md5 == self.md5).group_by(Booru.id)
 
 
 # ## INITIALIZATION

app/controllers/archives_controller.py
@@ -9,8 +9,8 @@ from ..models import Archive
 from ..logical.database.archive_db import update_archive_from_parameters
 from ..logical.records.booru_rec import recreate_archived_booru, relink_archived_booru
 from ..logical.records.artist_rec import recreate_archived_artist, relink_archived_artist
-from ..logical.records.illust_rec import recreate_archived_illust, relink_archived_illust
-from ..logical.records.post_rec import recreate_archived_post, relink_archived_post
+from ..logical.records.illust_rec import recreate_archived_illust
+from ..logical.records.post_rec import recreate_archived_post
 from .base_controller import show_json_response, index_json_response, search_filter, process_request_values,\
     get_params_value, paginate, default_order, get_or_abort, index_html_response
 
@@ -29,8 +29,6 @@ RECREATE_ARCHIVE_FUNCS = {
 RELINK_ARCHIVE_FUNCS = {
     'booru': relink_archived_booru,
     'artist': relink_archived_artist,
-    'illust': relink_archived_illust,
-    'post': relink_archived_post,
 }
 
 # #### Load options

app/controllers/subscription_elements_controller.py
@@ -9,13 +9,11 @@ from utility.data import eval_bool_string
 from utility.time import days_from_now
 
 # ## LOCAL IMPORTS
-from ..models import SubscriptionElement, IllustUrl, Illust, Artist
+from ..models import SubscriptionElement, IllustUrl, Illust, Artist, ArchivePost
 from ..logical.utility import search_url_for, set_error
 from ..logical.database.base_db import commit_session
 from ..logical.database.subscription_element_db import get_elements_by_id,\
     update_subscription_element_from_parameters
-from ..logical.database.post_db import get_posts_by_md5s, get_post_by_md5
-from ..logical.database.archive_db import get_archives_by_post_md5s, get_archive_by_post_md5
 from ..logical.records.subscription_rec import redownload_element, reinstantiate_element, relink_element,\
     create_elements_from_source
 from .base_controller import show_json_response, index_json_response, search_filter, process_request_values,\
@@ -30,9 +28,9 @@ bp = Blueprint("subscription_element", __name__)
 # #### Load options
 
 INDEX_HTML_OPTIONS = (
-    selectinload(SubscriptionElement.illust_url).selectinload(IllustUrl.illust).options(
-        selectinload(Illust.artist).selectinload(Artist.site_account),
-        selectinload(Illust.urls).lazyload('*'),
+    selectinload(SubscriptionElement.illust_url).options(
+        selectinload(IllustUrl.illust).selectinload(Illust.artist).selectinload(Artist.site_account),
+        selectinload(IllustUrl.archive_post).selectinload(ArchivePost.archive),
     ),
     selectinload(SubscriptionElement.post).lazyload('*'),
     selectinload(SubscriptionElement.errors),
@@ -106,20 +104,6 @@ def index_html():
             and page > elements.pages:
         return redirect(url_for('subscription_element.index_html', page=elements.pages,
                                 **{k: v for (k, v) in request.args.items() if k != 'page'}))
-    missing_md5s = set(element.md5 for element in elements.items
-                       if element.post is None and element.status_name in ['unlinked', 'duplicate'])
-    missing_posts = get_posts_by_md5s(list(missing_md5s)) if len(missing_md5s) else []
-    archive_md5s = set(element.md5 for element in elements.items if element.status_name == 'archived')
-    archives = get_archives_by_post_md5s(list(archive_md5s)) if len(archive_md5s) else []
-    for item in elements.items:
-        post_match = None
-        if item.md5 in missing_md5s:
-            post_match = next((post for post in missing_posts if post.md5 == item.md5), None)
-        setattr(item, 'post_match', post_match)
-        archive_match = None
-        if item.md5 in archive_md5s:
-            archive_match = next((archive for archive in archives if archive.subdata.md5 == item.md5), None)
-        setattr(item, 'archive_match', archive_match)
     return render_template_ws("subscription_elements/index.html",
                               page=elements,
                               subscription_element=SubscriptionElement())
@@ -233,9 +217,6 @@ def relink_json(id):
 # #### Private functions
 
 def _json_preview(results, element):
-    if element.post is None:
-        element.post_match = get_post_by_md5(element.md5)
-        element.archive_match = get_archive_by_post_md5(element.md5) if element.post_match is None else None
     results['item'] = element.to_json()
     results['html'] = render_template_ws("subscription_elements/_element_preview.html", element=element)
     return results

app/helpers/archives_helper.py
@@ -78,10 +78,6 @@ def set_temporary_link(archive):
 
 
 def has_relink(archive):
-    if archive.type_name == 'post' and archive.subdata.illusts is not None:
-        return True
-    if archive.type_name == 'illust' and any(url_data['md5'] for url_data in archive.subdata.urls_json):
-        return True
     if archive.type_name == 'artist' and archive.subdata.boorus is not None:
         return True
     if archive.type_name == 'booru' and archive.subdata.artists is not None:

app/helpers/subscriptions_helper.py
@@ -225,10 +225,10 @@ def keep_element_val(subscription_element):
 # ###### Link functions
 
 def element_preview_link(element, lazyload):
-    if element.post_match is not None:
-        return post_preview_link(element.post_match, lazyload)
-    if element.archive_match is not None:
-        return archive_preview_link(element.archive_match, lazyload)
+    if element.post is not None:
+        return post_preview_link(element.post, lazyload)
+    if element.archive_post is not None:
+        return archive_preview_link(element.archive_post.archive, lazyload)
     preview_url = element.illust_url.preview_url
     title = f"( {element.illust_url.width} x  {element.illust_url.height} )"
     addons = {

app/logical/database/archive_db.py
@@ -46,14 +46,6 @@ def get_archive_by_post_md5(md5):
     return Archive.query.join(ArchivePost).filter(ArchivePost.md5 == md5).one_or_none()
 
 
-def get_archives_by_post_md5s(data_keys):
-    archive_posts = []
-    for i in range(0, len(data_keys), 100):
-        sublist = data_keys[i: i + 100]
-        archive_posts += Archive.query.join(ArchivePost).filter(ArchivePost.md5.in_(sublist)).all()
-    return archive_posts
-
-
 def get_archive_by_illust_site(site_id, site_illust_id):
     return Archive.query.join(ArchiveIllust).filter(ArchiveIllust.site_id == site_id,
                                                     ArchiveIllust.site_illust_id == site_illust_id)\

app/logical/database/archive_post_db.py
@@ -8,8 +8,7 @@ from .base_db import set_column_attributes, save_record, set_timesvalue
 # ## GLOBAL VARIABLES
 
 ANY_WRITABLE_ATTRIBUTES = ['md5', 'width', 'height', 'file_ext', 'size', 'danbooru_id', 'created', 'type_id',
-                           'pixel_md5', 'duration', 'audio', 'tags_json', 'notations_json', 'errors_json',
-                           'illusts_json']
+                           'pixel_md5', 'duration', 'audio', 'tags_json', 'notations_json', 'errors_json']
 NULL_WRITABLE_ATTRIBUTES = ['archive_id']
 
 

app/logical/database/illust_url_db.py
@@ -33,14 +33,3 @@ def set_illust_url_from_parameters(illust_url, setparams, action, commit):
     if set_column_attributes(illust_url, ANY_WRITABLE_ATTRIBUTES, NULL_WRITABLE_ATTRIBUTES, setparams):
         save_record(illust_url, action, commit=commit)
     return illust_url
-
-
-# #### Query
-
-def get_illust_url_by_full_url(full_url):
-    from ..sites import site_name_by_url
-    from ..sources import source_by_site_name
-    site_name = site_name_by_url(full_url)
-    source = source_by_site_name(site_name)
-    partial = source.partial_media_url(full_url)
-    return IllustUrl.query.filter(IllustUrl.site_value == site_name, IllustUrl.url == partial).one_or_none()

commit 60b7dc7d9e7b2d2ed486f6944c4fe85a150e7303

Fix post json
Reversion caused by ad7956bef64b13b2033038e729f1ac8163db2289.

app/models/post.py
@@ -229,6 +229,10 @@ class Post(JsonModel):
         return next((element for element in self.subscription_elements if element.status_name == 'active'), None)
 
     @property
+    def illust_urls_json(self):
+        return [dict_prune(illust_url.to_json(), ['id', 'post_id']) for illust_url in self.illust_urls]
+
+    @property
     def errors_json(self):
         return [dict_filter(error.to_json(), ['module', 'message', 'created']) for error in self.errors]
 